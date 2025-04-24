19b85ec66999059d1954ae2e59cf41498184d253
matiasq22 <matiasmartinezquevedo22@gmail.com>
2025-04-24 15:42:57 -0300

    Revert "initial commit"
    This reverts commit fe2a8cc65b6b05e598a34cd2b7b6cdcb82822c9b.

--- config/graphmail.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-return [
-    'client_id'     => env('AZURE_CLIENT_ID'),
-    'client_secret' => env('AZURE_CLIENT_SECRET'),
-    'tenant_id'     => env('AZURE_TENANT_ID'),
-    'from'          => env('GRAPH_FROM_ADDRESS'),
-];
